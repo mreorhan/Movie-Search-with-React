3996abd52364362b5e5f9c4dd1294fa0afddbf24
mreorhan <videokod7@gmail.com>
2018-02-25 11:19:07 +0300

    redux eklendi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import registerServiceWorker from './registerServiceWorker';
 //import { BrowserRouter as Router   } from 'react-router-dom';
 ReactDOM.render(
